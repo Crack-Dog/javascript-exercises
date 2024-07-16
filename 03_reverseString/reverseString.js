f64a85b3bb72dfb037d0c0ba72ca6596cffb58c2
Wesley Mak <wesleymak005@gmail.com>
2024-07-16 13:08:00 -0400

    Finish 02 & 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseString = "";
+    const splitArray = string.split("");
+    splitArray.reverse();
 
+    for (i = 0; i < splitArray.length; i++){
+        reverseString += splitArray[i]
+    }
+    return reverseString
 };
 
 // Do not edit below this line
