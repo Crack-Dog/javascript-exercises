66e5c642c1a24b2053d612091654aad6fd13a97e
Wesley Mak <wesleymak005@gmail.com>
2024-07-19 16:17:20 -0400

    Finish 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    let num1 = 0;
+    let num2 = 1;
+    if (value == 1){
+        return num2;
+    }
+    else if (value == 0){
+        return num1;
+    }
+    else if (value == 2){
+        return num2;
+    }
+    else if (value < 0){
+        return "OOPS"
+    }
+    else {
+        for (let i = 2; i <= value; i++){
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num2;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
