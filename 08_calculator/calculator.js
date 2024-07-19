04104a8b51aa9bceb1a7f72a64fb53893476a2ae
Wesley Mak <wesleymak005@gmail.com>
2024-07-19 12:46:55 -0400

    finish 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(val, power) {
+	return val ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  if (val ===0){
+    return 1;
+  }
+  let total = 1;
+  for (let i = val; i > 0; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
