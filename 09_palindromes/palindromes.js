8cdb363beb45ff66c2386983663f5927a6f36a39
Wesley Mak <wesleymak005@gmail.com>
2024-07-19 15:39:29 -0400

    Finih 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let newString = string.toLowerCase().split("").filter((character) => alphanumerical.includes(character)).join("");
+    let reverseString = newString.split("").reverse().join("");
+
+    if (newString === reverseString){
+        return true
+    }
+    return false
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
