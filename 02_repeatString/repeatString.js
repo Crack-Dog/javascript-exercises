ede10505a5564e8238e525196cd6192a698d9891
Wesley Mak <wesleymak005@gmail.com>
2024-07-19 23:29:00 -0400

    misc

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    if (num < 0){
+        return "ERROR";
+    }
+    
+    let newString = ""
+    if (num > 0){
+        for (let i = 0; i < num; i++){
+            newString += string;
+        }
+    }
+    return newString;
 };
+
+repeatString("bob", -1);
 
 // Do not edit below this line
 module.exports = repeatString;
