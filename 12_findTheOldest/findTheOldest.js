6e3f3e963580fcc2160d49588539ac5112e3cfff
Wesley Mak <wesleymak005@gmail.com>
2024-07-19 23:28:45 -0400

    finish 12 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+function getAge(dob, dod){
+    if (dod === undefined){
+        dod = new Date().getFullYear();
+    }
+    return dod - dob
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (currentAge > oldestAge){
+            return currentPerson
+        }
+        else {
+            return oldest
+        }
+        });
 };
 
 // Do not edit below this line
